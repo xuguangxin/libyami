8196a4b7c18c54677201b8807034f1d34f501898
Xu Guangxin <guangxin.xu@intel.com>
2016-02-18 16:09:20 +0800

    tests: try open renderD128 before card0
    card0 is exclusive, if x11 use it we can't get libva library.
    so we'd better try renderD128 first

--- tests/vppinputoutput.cpp
+++ tests/vppinputoutput.cpp
@@ -43,9 +43,11 @@ using namespace YamiMediaCodec;
 SharedPtr<VADisplay> createVADisplay()
 {
     SharedPtr<VADisplay> display;
-    int fd = open("/dev/dri/card0", O_RDWR);
+    int fd = open("/dev/dri/renderD128", O_RDWR);
+    if (fd < 0)
+        fd = open("/dev/dri/card0", O_RDWR);
     if (fd < 0) {
-        ERROR("open card0 failed");
+        ERROR("can't open drm device");
         return display;
     }
     VADisplay vadisplay = vaGetDisplayDRM(fd);
