5258544365dfb9862e30a043eca7a1f6cc4ff596
XuGuangxin <Guangxin.Xu@intel.com>
2016-08-29 23:43:23 +0800

    h264dec: use ensureSurfacePool and ensureProfile

--- decoder/vaapidecoder_h264.h
+++ decoder/vaapidecoder_h264.h
@@ -144,7 +144,7 @@ private:
     void fillReferenceIndexForList(VASliceParameterBufferH264* sliceParam,
                                    const SliceHeader* const slice,
                                    RefSet& refSet, bool isList0);
-    bool isDecodeContextChanged(const SharedPtr<SPS>& sps);
+    YamiStatus checkDecodeContextChange(const SharedPtr<SPS>& sps);
     bool decodeAvcRecordData(uint8_t* buf, int32_t bufSize);
 
     YamiStatus createPicture(const SliceHeader* const,
